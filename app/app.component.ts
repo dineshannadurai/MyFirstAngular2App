0b7e6ecbb675a490ca9e85f3389bb9550e49d088
Sumither Selvaraj <Sumither Selvaraj>
2017-01-27 00:54:56 +0530

    Routes, Service samples

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <ul>
+  	<li><a routerLink="/">Home</a></li>
+  	<li><a routerLink="/about">About</a></li>
+  </ul>
+  <router-outlet></router-outlet>`,
 })
-export class AppComponent  { name = 'Dinesh'; }
+export class AppComponent  {}
---
+++ app/components/user.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { PostService } from '../services/post.service'
+
+@Component({
+	moduleId:module.id;
+  	selector: 'user',
+  	templateUrl: 'user.component.html',
+  	providers:[PostService]
+})
+export class UserComponent  
+{ 
+	name : string; 
+	email : string;
+	address :address;
+	hobbies :string[];
+	showHobbies :boolean;
+	posts:post[];
+
+	constructor(private postService: PostService){
+		this.name = 'Dinesh'; 
+		this.email = 'dinesh@emdsys.com';
+		this.address = {
+			street:'Ambrosa apartment',
+			city:'Chennai',
+			state:'TN'
+		};
+		this.hobbies = ['Tennis', 'Cricket','Workout'];
+		this.showHobbies = false;
+		this.postService.getPost().subscribe(posts=>{
+			this.posts = posts;
+		});
+	}
+	toogleHobbies(){
+		this.showHobbies = !this.showHobbies;
+	}
+	addHobby(hobby){
+		this.hobbies.push(hobby);
+	}
+	deleteHobby(index){
+		this.hobbies.splice(index,1);
+	}
+}
+interface address{
+	street : string,
+	city : string,
+	state : string
+}
+interface post{
+	body:string,
+	id:number,
+	title:string
+}
